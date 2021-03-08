6f013e065c7b0cb4738c586eff57c26c55fccb19
Reza Bidar <reza.smart306@gmail.com>
2021-03-09 00:17:14 +0330

    make authorization for cart controller

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -1,9 +1,15 @@
 <?php
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class CartController provides all shopping cart features
+ * @package App\Controller
+ * @IsGranted("ROLE_USER")
+ */
 class CartController extends AbstractController
 {
 
